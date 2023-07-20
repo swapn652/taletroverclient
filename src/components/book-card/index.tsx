7ff60bbad2c3671aa73cf1c11adb4e87c19fa951
SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
2023-07-20 14:56:09 +0530

    updated book card

--- src/components/book-card/index.tsx
+++ src/components/book-card/index.tsx
@@ -3,6 +3,10 @@ import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
 interface BookCardProps {
   imageSrc: string;
@@ -10,6 +14,16 @@ interface BookCardProps {
 }
 
 const BookCard: React.FC<BookCardProps> = ({ imageSrc, title }) => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Card
       sx={{
@@ -18,7 +32,7 @@ const BookCard: React.FC<BookCardProps> = ({ imageSrc, title }) => {
         borderRadius: 8,
         overflow: "hidden",
         textAlign: "center",
-        backgroundColor: "#fff", // Set the background color to match the theme
+        backgroundColor: "#fff",
         transition: "transform 0.2s ease-in-out",
         "&:hover": {
           transform: "scale(1.05)",
@@ -37,8 +51,35 @@ const BookCard: React.FC<BookCardProps> = ({ imageSrc, title }) => {
           {title}
         </Typography>
       </Box>
+      <IconButton
+        aria-controls="book-card-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+        sx={{ position: "absolute", top: 0, right: 0 }}
+      >
+        <MoreVertIcon />
+      </IconButton>
+      <Menu
+        id="book-card-menu"
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        <MenuItem onClick={handleClose}>Edit</MenuItem>
+        <MenuItem onClick={handleClose}>Delete</MenuItem>
+        <MenuItem onClick={handleClose}>Show</MenuItem>
+      </Menu>
     </Card>
   );
 };
 
 export default BookCard;
+
