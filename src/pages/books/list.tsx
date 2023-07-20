56f3b8264b00cdfbe9c7dbe4c26cc8c62916bcf9
SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
2023-07-20 20:28:02 +0530

    Added a button to create new book

--- src/pages/books/list.tsx
+++ src/pages/books/list.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { IResourceComponentsProps, useList } from "@refinedev/core";
-import { CircularProgress } from "@mui/material";
+import { CircularProgress, Button } from "@mui/material";
 import BookCard from "../../components/book-card";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
+
 export const BooksList: React.FC<IResourceComponentsProps> = () => {
   const { data, isLoading, isError } = useList<any>({ resource: "books" });
 
@@ -23,10 +25,18 @@ export const BooksList: React.FC<IResourceComponentsProps> = () => {
   }
 
   return (
-    <div style={{ display: "grid", gap: "20px", gridTemplateColumns: "repeat(auto-fit, 200px)" }}>
+    <div style={{ display: "flex", flexWrap: "wrap", gap: "20px", padding: "20px", position: "relative" }}>
       {data.data.map((book: any) => (
         <BookCard bookId={book._id} key={book._id} imageSrc="/taletrove-logo.png" title={book.title} />
       ))}
+      {/* Create New Book Button */}
+      <div style={{ position: "absolute", bottom: "-30px", left: "20px" }}>
+        <Link to="/addBook">
+          <Button variant="contained" color="primary">
+            Create New Book
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 };
